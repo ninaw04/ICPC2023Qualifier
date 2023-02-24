254f465ee6ac9e44c4865101dbb6c0ee352bc946
Viha Shah <47764272+Viha123@users.noreply.github.com>
2023-02-24 10:47:39 -0800

    Update DoublePassword.java
    :sparkles: to note: need to catch exception, or else it won't work

--- DoublePassword/DoublePassword.java
+++ DoublePassword/DoublePassword.java
@@ -1,30 +1,31 @@
-package pacnw_practice;
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Scanner;
 
 public class DoublePassword {
-	private static final Reader Reader = null;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args){
 		//Scanner sc = new Scanner(System.in);
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		String pswd1 = sc.readLine();
-		
-		String pswd2 = sc.readLine();
-//		while(())
-		
-//		BufferedReader br = new BufferedReader(Reader in);
-		int count = 1;
-		for(int i = 0; i < 4; i++) {
-			if (pswd1.charAt(i) != pswd2.charAt(i)) {
-				count *= 2;
+		try{
+			BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+			String pswd1 = sc.readLine();
+			
+			String pswd2 = sc.readLine();
+			int count = 1;
+			for(int i = 0; i < 4; i++) {
+				if (pswd1.charAt(i) != pswd2.charAt(i)) {
+					count *= 2;
+				}
 			}
+			System.out.print(count);
 		}
+	
+		catch(IOException e){
+			e.printStackTrace();
+		}
+//		BufferedReader br = new BufferedReader(Reader in);
 		
-//		System.out.println((int)Math.pow(2, count));
-		System.out.print(count);
 	}
 }
